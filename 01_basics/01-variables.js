ab74667d6f8ea1198caf963129a2ca77c86da650
sanjitasendanglimbu <154046466+sanjitasendanglimbu@users.noreply.github.com>
2024-04-28 03:19:21 +0000

    datatype

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,18 @@
+const accountId  = 144333
+let accountEmail = "disha@gmail.com"
+var accountPassword = "123456"
+accountCity = "kathmandu"
+let accountState;
+// accountId = 2
+
+accountEmail = "dih123@google.com"
+accountPassword ="asdfg2"
+accountCity ="tpj"
+
+console.log(accountId);
+/*
+prefer not to use var
+because of issusein block scope and function scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datetype.js
@@ -0,0 +1,23 @@
+" use strict"; //treat all js code as newer version
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3+3); // code readabillity should be high
+
+console.log("sanjita")
+
+let name = "sanjita"
+let age = 22
+let contact = 9845451473
+
+// number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigend
+// symbol 
+
+
+//oject
+
+console.log(typeof contact)
