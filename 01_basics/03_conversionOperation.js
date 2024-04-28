abc9e8f8fff305abb72be926c5f798b68fd271e9
sanjitasendanglimbu <154046466+sanjitasendanglimbu@users.noreply.github.com>
2024-04-28 17:11:03 +0000

    operations datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,14 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 let login = true
-console.log(typeof login);
+// console.log(typeof login);
 
 // "33" => 33
 // "33abc" => NaN
@@ -17,7 +17,7 @@ console.log(typeof login);
 let isLoggedIn = "sanjita"
 
 let booleanIsLoggendIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggendIn);
+// console.log(booleanIsLoggendIn);
 
 // 1 => true; 0=> false
 // "" => false
@@ -27,5 +27,27 @@ console.log(booleanIsLoggendIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****** Operations ********
+
+let value = 3;
+ 
+let numValue = -value;
+// console.log(numValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2); 
+// console.log(2**2); power 
+// console.log(5/2);
+// console.log(5%2);
+
+console.log(5 > 2);
+console.log(5 <= 2);
+
+let gameCounter = 100
+gameCounter++;
+// perfix and postfix
+// console.log(gameCounter);
