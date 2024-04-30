ec07427deb0f07ce5ccff866cd85a07dd402f472
sanjitasendanglimbu <154046466+sanjitasendanglimbu@users.noreply.github.com>
2024-04-30 04:13:36 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,15 @@
+//Dates
+
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2033, 0, 23)
+let myCreatedDate = new Date("06-01-2002")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+console.log(Date.now());
